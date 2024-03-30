314eaad714e568b7770b0cdce7e4989bab168605
sukrit <101797008+Sukrit-garg@users.noreply.github.com>
2024-03-30 17:11:16 +0530

    Add files via upload

---
+++ model.py
@@ -0,0 +1,93 @@
+from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain.prompts import PromptTemplate
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_community.llms import CTransformers
+from langchain.chains import RetrievalQA
+import chainlit as cl
+
+DB_FAISS_PATH = 'vectorstore/db_faiss'
+
+custom_prompt_template = """Only based on the given context respond to the given Question in easy to understand manner.
+
+Context: {context}
+Question: {question}
+
+Helpful answer:
+"""
+
+def set_custom_prompt():
+    """
+    Prompt template for QA retrieval for each vectorstore
+    """
+    prompt = PromptTemplate(template=custom_prompt_template,
+                            input_variables=['context', 'question'])
+    return prompt
+
+#Retrieval QA Chain
+def retrieval_qa_chain(llm, prompt, db):
+    qa_chain = RetrievalQA.from_chain_type(llm=llm,
+                                       chain_type='stuff',
+                                       retriever=db.as_retriever(search_kwargs={'k': 2}),
+                                       return_source_documents=True,
+                                       chain_type_kwargs={'prompt': prompt}
+                                       )
+    return qa_chain
+
+#Loading the model
+def load_llm():
+    # Load the locally downloaded model here
+    llm = CTransformers(
+        model = "llama-2-7b-chat.ggmlv3.q8_0.bin",
+        model_type="llama",
+        max_new_tokens = 512,
+        temperature = 0.3
+    )
+    return llm
+
+#QA Model Function
+def qa_bot():
+    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2",
+                                       model_kwargs={'device': 'cpu'})
+    db = FAISS.load_local(DB_FAISS_PATH, embeddings, allow_dangerous_deserialization=True)
+    llm = load_llm()
+    qa_prompt = set_custom_prompt()
+    qa = retrieval_qa_chain(llm, qa_prompt, db)
+
+    return qa
+
+#output function
+def final_result(query):
+    qa_result = qa_bot()
+    response = qa_result({'query': query})
+    return response
+
+#chainlit code
+@cl.on_chat_start
+async def start():
+    chain = qa_bot()
+    msg = cl.Message(content="Callin your Study Buddy...")
+    await msg.send()
+    msg.content = "Hi, Welcome to Chatml. What do you want to know about?"
+    await msg.update()
+
+    cl.user_session.set("chain", chain)
+
+@cl.on_message
+async def main(message: cl.Message):
+    chain = cl.user_session.get("chain") 
+    cb = cl.AsyncLangchainCallbackHandler(
+        stream_final_answer=True, answer_prefix_tokens=["FINAL", "ANSWER"]
+    )
+    cb.answer_reached = True
+    res = await chain.acall(message.content, callbacks=[cb])
+    answer = res["result"]
+    sources = res["source_documents"]
+
+    if sources:
+        answer += f"\n\n\nSources: -\n  " + str(sources)
+    else:
+        answer += "\nNo sources found"
+
+    await cl.Message(content=answer).send()
+
